28dabb215b5a934dfb9ad0e1afcd85ead965ab82
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-11-11 22:01:41 -0300

    configurando o restcontroller e getmapping

---
+++ src/test/java/org/senai/prjprodutosjava/PrjProdutosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.prjprodutosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjProdutosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
